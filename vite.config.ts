01c62748bed60f58c5aa45fe9542bf0cd45b1227
aldrinbf <aldrinfernandez51@gmail.com>
2026-03-24 07:26:05 +0800

    unpublish but can worked locally

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   // base: '/Demano-s-Medical-and-Pediatric-Clinic/';
   // const env = loadEnv(mode, '.', '');
-  // const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react(), tailwindcss()],
     define: {
